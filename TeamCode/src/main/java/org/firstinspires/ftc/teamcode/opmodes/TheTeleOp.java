865f06c099fac71016b3b9f80c2dd4059e38db9e
suhas <hi@suhas.one>
2023-12-02 13:19:41 -0600

    add intakeMotor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TheTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TheTeleOp.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 
 import org.firstinspires.ftc.teamcode.lib.MecanumDrivetrain;
@@ -9,9 +10,11 @@ import org.firstinspires.ftc.teamcode.lib.MecanumDrivetrain;
 @TeleOp(name = "MAIN TeleOp")
 public class TheTeleOp extends LinearOpMode {
     private MecanumDrivetrain drivetrain;
+    private DcMotor intakeMotor;
     @Override
     public void runOpMode() {
         drivetrain = new MecanumDrivetrain(hardwareMap, telemetry);
+        intakeMotor = hardwareMap.get(DcMotor.class, "ils");
 
         waitForStart();
 
@@ -20,6 +23,12 @@ public class TheTeleOp extends LinearOpMode {
             double power = -gamepad1.left_stick_y;
             double strafe = gamepad1.left_stick_x;
             double turn = gamepad1.right_stick_x;
+
+            if (gamepad2.x) {
+                intakeMotor.setPower(0.5);
+            } else {
+                intakeMotor.setPower(0);
+            }
 
             drivetrain.drive(power, strafe, turn);
 
